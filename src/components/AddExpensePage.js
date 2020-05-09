70dee01a7efd604c2c90871e6059d0e32d76adc1
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-05-08 22:40:10 -0400

    routes

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddExpensePage = () => (
+    <div>
+        This is from my Add expense
+    </div>
+)
+
+export default AddExpensePage
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboardcomponent
+    </div>
+)
+
+export default ExpenseDashboardPage
