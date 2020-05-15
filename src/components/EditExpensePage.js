63d052e5ea0a4ff417a50add427cbcd9f72b2081
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-05-15 01:16:13 -0400

    edit expenses

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import ExpenseForm from './ExpenseForm'
+import { connect } from 'react-redux'
+import { editExpense, removeExpense } from '../actions/expenses'
 
 const EditExpensePage = (props) => {
-    console.log(props)
     return (
         <div>
+            <ExpenseForm
+                expense={props.expense}
+                onSubmit={(expense) => {
+                    console.log('updated', expense);
+                    props.dispatch(editExpense(props.expense.id, expense))
+                    props.history.push('/')
+                }}    
+            />
+            <button onClick={() => {
+                props.dispatch(removeExpense({id: props.expense.id}))
+                props.history.push('/')
+            }}>Remove</button>
             Editing the expense with id of {props.match.params.id}
         </div>
     )
 }
 
-export default EditExpensePage
+const mapStatetoProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    }
+    
+}
+
+export default connect(mapStatetoProps)(EditExpensePage)
